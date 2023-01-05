df027e1893ab1ef401eb00eca69cb17def4d8d57
coheim <pari0130@gmail.com>
2023-01-06 00:20:06 +0900

    2023-01-06 home navi test

--- lib/presentatiton/pages/home/home_page.dart
+++ lib/presentatiton/pages/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'home_controller.dart';
+import 'package:food_haccp/config/fonts/index.dart';
+import 'package:food_haccp/presentatiton/widgets/index.dart';
+import 'package:food_haccp/config/themes/index.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({super.key});
@@ -9,20 +12,74 @@ class HomePage extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          controller.title,
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
         backgroundColor: Colors.white,
-      ),
-      body: Container(
-        child: Center(
-          child: Text(
-            "Home Page",
-            style: TextStyle(fontSize: 20),
-          ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 5, right: 5),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      height: 30,
+                      child: const Text(
+                        "아푸",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: gmarketSansTTFBold),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      fit: FlexFit.tight,
+                      child: GestureSearchBox(
+                        hint: "",
+                        suffix: Icon(Icons.search, color: darker),
+                        onTap: () {
+                          print("dd");
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+      body: getBody(),
+    );
+  }
+
+  getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 15, right: 15),
+            child: const Text(
+              "Find Your Meals",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     );
   }
